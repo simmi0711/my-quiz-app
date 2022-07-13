a7438dde6f5a96c675812a862986784d84584fe4
simmi0711 <105014239+simmi0711@users.noreply.github.com>
2022-07-13 20:35:37 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is correct about features of JavaScript?",
+    answer: "All of the above.",
+    options: [
+      "JavaScript is a lightweight, interpreted programming language",
+      "JavaScript is designed for creating network-centric applications",
+      "JavaScript is complementary to and integrated with Java.",
+      "All of the above."
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following function of String object returns the characters in a string between two indexes into the string?",
+    answer: "substring()",
+    options: [
+      "slice()",
+      "split()",
+      "substr()",
+      "substring()"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following function of String object creates an HTML anchor that is used as a hypertext target?",
+    answer: "anchor()",
+    options: [
+      "anchor()",
+      "link()",
+      "blink()",
+      "big()"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of the following function of Array object represents the source code of an object?",
+    answer: "toSource()",
+    options: [
+      "toSource()",
+      "splice()",
+      "toString()",
+      "unshift()"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which built-in method returns the calling string value converted to lower case?",
+    answer: "toLowerCase()",
+    options: [
+      "toLowerCase()",
+      "toLower()",
+      "changeCase(case)",
+      "None of the above."
+    ]
+  },
+  {
+    numb: 6,
+    question: "_________ keyword is used to declare variables in javascript.",
+    answer: "Var",
+    options: [
+      "Var",
+      "Dim",
+      "String",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which CSS property can provide to add an effect when changing from one style to another,without using Flash animations  or javascript?",
+    answer: "Transitions",
+    options: [
+      "Associations",
+      "Transitions",
+      "Transistor",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "When a JavaScript object is sent to Java, the runtime engine creates a Java wrapper of type",
+    answer: "JSObject",
+    options: [
+      "ScriptObject",
+      "JSObject",
+      "JavaObject",
+      "Jobject"
+   ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is not supported by older browsers?",
+    answer: "All of the above",
+    options: [
+      "CSS",
+      "Layers",
+      "Frames",
+      "All of the above"
+   ]
+},
+{
+   numb: 10,
+   question: "What do you add to a template in order to control where page content goes?",
+   answer: "Editable Regions",
+   options: [
+    "Text Frames",
+    "HTML Controllers",
+    "Editable Regions",
+    "Page Content Controllers"
+  ]
+},
+];
